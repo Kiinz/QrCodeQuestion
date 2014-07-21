ce3379fe3b137038212ac7a9c06dc7316181ba54
FHAndroidProjekt <FHAndroidProjekt@yopmail.com>
2014-07-21 15:19:22 +0200

    Start

--- src/at/klu/qrcodequest/StartActivity.java
+++ src/at/klu/qrcodequest/StartActivity.java
@@ -1,15 +1,14 @@
 package at.klu.qrcodequest;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class StartActivity extends Activity implements OnClickListener {
 
@@ -27,24 +26,6 @@ public class StartActivity extends Activity implements OnClickListener {
         userID = sha1(userID);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.start, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 
 	@Override
 	public void onClick(View v) {
