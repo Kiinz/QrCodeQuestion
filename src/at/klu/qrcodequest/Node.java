ec94d3304aeffdeb966e8a7a6c7bacb557368994
Kiinz <alexander.kainz@htl-klu.at>
2014-07-22 09:04:52 +0200

    Version36

--- src/at/klu/qrcodequest/Node.java
+++ src/at/klu/qrcodequest/Node.java
@@ -7,39 +7,76 @@ public class Node {
 	private int active;
 	private int sequence;
 	private int dtRegistration;
+	private String registrationTarget1;
+	private String registrationTarget2;
 	private String name;
 	private String description;
 	private String location;
 	
 	
+	
+	
+	public Node(int questPk, int active, int sequence, int dtRegistration,
+			String registrationTarget1, String registrationTarget2,
+			String name, String description, String location) {
+		super();
+		this.questPk = questPk;
+		this.active = active;
+		this.sequence = sequence;
+		this.dtRegistration = dtRegistration;
+		this.registrationTarget1 = registrationTarget1;
+		this.registrationTarget2 = registrationTarget2;
+		this.name = name;
+		this.description = description;
+		this.location = location;
+	}
+
 	public Node(int id, int questPk, int active, int sequence,
-			int registration, String name, String description, String location) {
+			int dtRegistration, String registrationTarget1,
+			String registrationTarget2, String name, String description,
+			String location) {
 		super();
 		this.id = id;
 		this.questPk = questPk;
 		this.active = active;
 		this.sequence = sequence;
-		this.dtRegistration = registration;
+		this.dtRegistration = dtRegistration;
+		this.registrationTarget1 = registrationTarget1;
+		this.registrationTarget2 = registrationTarget2;
 		this.name = name;
 		this.description = description;
 		this.location = location;
 	}
-	public Node(int questPk, int active, int sequence,
-			int registration, String name, String description, String location) {
-		super();
-		this.questPk = questPk;
-		this.active = active;
-		this.sequence = sequence;
-		this.dtRegistration = registration;
-		this.name = name;
-		this.description = description;
-		this.location = location;
-	}
-	
+
 	public Node() {
 		
 	}
 	
+	
+	public int getDtRegistration() {
+		return dtRegistration;
+	}
+
+	public void setDtRegistration(int dtRegistration) {
+		this.dtRegistration = dtRegistration;
+	}
+
+	public String getRegistrationTarget1() {
+		return registrationTarget1;
+	}
+
+	public void setRegistrationTarget1(String registrationTarget1) {
+		this.registrationTarget1 = registrationTarget1;
+	}
+
+	public String getRegistrationTarget2() {
+		return registrationTarget2;
+	}
+
+	public void setRegistrationTarget2(String registrationTarget2) {
+		this.registrationTarget2 = registrationTarget2;
+	}
+
 	public int getId() {
 		return id;
 	}
@@ -88,16 +125,15 @@ public class Node {
 	public void setLocation(String location) {
 		this.location = location;
 	}
+
 	@Override
 	public String toString() {
 		return "Node [id=" + id + ", questPk=" + questPk + ", active=" + active
 				+ ", sequence=" + sequence + ", dtRegistration="
-				+ dtRegistration + ", name=" + name + ", description="
+				+ dtRegistration + ", registrationTarget1="
+				+ registrationTarget1 + ", registrationTarget2="
+				+ registrationTarget2 + ", name=" + name + ", description="
 				+ description + ", location=" + location + "]";
 	}
-	
-	
-	
-	
 
 }
--- src/at/klu/qrcodequest/NodeMethodes.java
+++ src/at/klu/qrcodequest/NodeMethodes.java
@@ -13,8 +13,8 @@ public static ArrayList<Node> getNodesfromJSONString() throws JSONException{
 		
     	ArrayList <Node> nodes = new ArrayList<Node>();
     	
-    	System.out.println(HTTPHelper.makeGetRequest("http://192.168.136.84/nodes.html").toString());
-    	JSONObject obj = new JSONObject(HTTPHelper.makeGetRequest("http://192.168.136.84/nodes.html").toString());
+    	System.out.println(HTTPHelper.makeGetRequest("http://192.168.136.78/nodes.html").toString());
+    	JSONObject obj = new JSONObject(HTTPHelper.makeGetRequest("http://192.168.136.78/nodes.html").toString());
     	JSONArray array = obj.getJSONArray("Nodes");
     	
     	for (int i = 0; i < array.length(); i++){
@@ -24,20 +24,18 @@ public static ArrayList<Node> getNodesfromJSONString() throws JSONException{
     		String description = node.getString("description");
     		String name = node.getString("name");
     		String location = node.getString("location");
+    		String registrationTarget1 = node.getString("registrationTarget1");
+    		String registrationTarget2 = node.getString("registrationTarget2");
     		int questPk = node.getInt("questPk");
     		int active = node.getInt("active");
     		int sequence = node.getInt("sequence");
     		int dtRegistration = node.getInt("dtRegistration");
     		int id = node.getInt("id");
     		
-    		
-    		
-    		
-    		Node quest1 = new Node(id, questPk, active, sequence, dtRegistration, name, description, location);
+    		Node quest1 = new Node(id,questPk,active,sequence,dtRegistration,registrationTarget1,registrationTarget2,name,description,location);
     		nodes.add(quest1);
 	}
     	return nodes;
 }
 }
-
 
