8c3c172b2fa0ace98f98ba359e848e25dab1eb07
Kiinz <alexander.kainz@htl-klu.at>
2014-07-29 17:13:51 +0200

    Version56

---
+++ src/at/klu/qrcodequest/EnableGPSorWLAN.java
@@ -0,0 +1,164 @@
+package at.klu.qrcodequest;
+
+import android.app.AlertDialog;
+import android.app.Service;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.LocationManager;
+import android.net.wifi.WifiManager;
+import android.os.IBinder;
+import android.widget.Toast;
+
+public class EnableGPSorWLAN extends Service{
+	
+	Context context;
+	LocationManager locationManager;
+	WifiManager wifiManager;
+	
+	boolean GPSenabled = false;
+	boolean WIFIenabled = false;
+	String selected;
+
+public EnableGPSorWLAN(Context context) {
+		super();
+		this.context = context;
+	}
+
+public boolean isGPSenabled(){
+	locationManager = (LocationManager) context.getSystemService(LOCATION_SERVICE);
+	GPSenabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+	
+	return GPSenabled;
+}
+
+public boolean isWIFIEnabled(){
+	wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+	WIFIenabled = wifiManager.isWifiEnabled();
+	return WIFIenabled;
+}
+
+public void enableGPS(){
+		
+		AlertDialog.Builder builder = new AlertDialog.Builder(context);
+    	
+    	builder.setTitle("Einstellungen �ndern");
+    	builder.setMessage("GPS ist nicht eingeschaltet. Wollen sie die Einstellungen �ndern?");
+    	
+    	builder.setPositiveButton("Einstellungen", new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				Intent intent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+				context.startActivity(intent);
+				
+			}
+		});
+    	builder.setNegativeButton("Beenden", new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+			
+				
+			}
+		});
+    	AlertDialog dialog = builder.create();
+    	dialog.show();
+    }
+
+public void enableNetwork(){
+	AlertDialog.Builder builder = new AlertDialog.Builder(context);
+	
+	builder.setTitle("Einstellungen �ndern");
+	builder.setMessage("Mit WLAN kann eine besser Genauigkeit erreicht werden. Wollen Sie WLAN nun einschalten?");
+	
+	builder.setPositiveButton("Einstellungen", new DialogInterface.OnClickListener() {
+		
+		@Override
+		public void onClick(DialogInterface dialog, int which) {
+			
+			Intent intent = new Intent (android.provider.Settings.ACTION_WIRELESS_SETTINGS);
+			context.startActivity(intent);
+			
+		}
+	});
+	
+	builder.setNegativeButton("Abbrechen", new DialogInterface.OnClickListener() {
+		
+		@Override
+		public void onClick(DialogInterface dialog, int which) {
+			
+			
+		}
+	});
+	
+	AlertDialog dialog = builder.create();
+	
+	dialog.show();
+}
+
+public void enableAll(){
+AlertDialog.Builder builder = new AlertDialog.Builder(context);
+	
+	builder.setTitle("Einstellungen Ändern");
+	
+	
+	 final CharSequence [] array = {"GPS","GPS + WLAN","WLAN"};
+	 selected = "GPS + WLAN";
+	    
+	 builder.setSingleChoiceItems(array, 1, new DialogInterface.OnClickListener() {
+		
+		@Override
+		public void onClick(DialogInterface dialog, int which) {
+			// TODO Auto-generated method stub
+			selected = array[which].toString();
+			
+		}
+	})
+	           
+	.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+		
+		@Override
+		public void onClick(DialogInterface dialog, int which) {
+			
+			if (selected.equals("GPS")){
+				Intent intent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+				context.startActivity(intent);
+			}
+			if(selected.equals("GPS + WLAN")){
+				wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+				wifiManager.setWifiEnabled(true);
+				Toast.makeText(context, "WIFI wurde automatisch eingeschalten", Toast.LENGTH_LONG).show();
+				Intent intent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+				context.startActivity(intent);
+			}
+			if(selected.equals("WLAN")){
+				wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+				wifiManager.setWifiEnabled(true);
+				Toast.makeText(context, "WIFI wurde automatisch eingeschalten", Toast.LENGTH_LONG).show();
+				
+			}
+			
+		}
+	})
+	
+	.setNegativeButton("Abbrechen", new DialogInterface.OnClickListener() {
+		
+		@Override
+		public void onClick(DialogInterface dialog, int which) {
+			
+			
+		}
+	});
+	
+	AlertDialog dialog = builder.create();
+	
+	dialog.show();
+}
+
+@Override
+public IBinder onBind(Intent intent) {
+	// TODO Auto-generated method stub
+	return null;
+}
+}
--- src/at/klu/qrcodequest/MainActivity.java
+++ src/at/klu/qrcodequest/MainActivity.java
@@ -4,9 +4,23 @@ import java.util.ArrayList;
 
 import org.json.JSONException;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.plus.model.people.Person.PlacesLived;
+
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -15,23 +29,91 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	private GoogleMap map;
     Button btscan;
     String result;
     int questPk = 0;
     int nodePk = 0;
+    EnableGPSorWLAN enable;
+    ArrayList<Node> nodes;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         AppDown.register(this);
+        
+        context = this;
+        
+        //GoogleMaps
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        
+        //Abfragen ob die Map erstellt werden konnte
+        if(map == null){
+			Toast.makeText(getApplicationContext(), "Die Karte konnte nicht erstellt werden", Toast.LENGTH_LONG).show();
+		}else{
+			map.setMyLocationEnabled(true);
+			
+			abfrage();
+
+		}
 
         btscan = (Button) findViewById(R.id.weiter);
         Bundle bundle = getIntent().getExtras();
         questPk = bundle.getInt("questPk");
 
+        //Thread für die Abfrage der Nodes
+        Thread thread = new Thread (){
+				public void run() {
+					
+					nodes = new ArrayList<Node>();
 
+	                try {
+	                    nodes = QuestsMethodes.getNodes(questPk);
+	                } catch (JSONException e) {
+	                    // TODO Auto-generated catch block
+	                    e.printStackTrace();
+	                }
+	                
+	                for (int i = 0; i < nodes.size(); i++){
+	                	if(nodes.get(i).getLocation() != null){
+	                		String s = nodes.get(i).getLocation();
+	                		System.out.println("" + s);
+	                		if (s.charAt(0) == '@'){
+	                			String locationString = s.substring(1);
+	                			
+	                			String [] location = new String [2];
+	                			
+	                			location = locationString.split(", ");
+	                		
+	                			
+	                			System.out.println(location [0] + "  " + location [1]);
+	                			
+	                			final int position = i;
+	                			final double latitude = Double.parseDouble(location[0]);
+	                			final double longitude = Double.parseDouble(location[1]);
 
+	                			Handler handler = new Handler(context.getMainLooper());
+	                			handler.post(new Runnable(){
+
+									@Override
+									public void run() {
+										placeMarker(latitude,longitude, nodes.get(position).getName());
+										
+									}
+	                				
+	                			});
+	                		}
+	                	}
+	                }
+					
+	               
+				}
+        };
+        
+        thread.start();
         //in some trigger function e.g. button press within your code you should add:
         btscan.setOnClickListener(new OnClickListener() {
             @Override
@@ -80,10 +162,10 @@ public class MainActivity extends Activity {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
-                System.out.println("" + nodes.get(0));
-                System.out.println("" + nodes.get(1));
-                System.out.println("" + nodes.get(2));
-                System.out.println("" + nodes.get(3));
+//                System.out.println("" + nodes.get(0));
+//                System.out.println("" + nodes.get(1));
+//                System.out.println("" + nodes.get(2));
+//                System.out.println("" + nodes.get(3));
 
 
                 for (int i = 0; i < nodes.size(); i++) {
@@ -107,5 +189,43 @@ public class MainActivity extends Activity {
             }
         }
     }
+    
+   
+		public void abfrage(){
+			EnableGPSorWLAN enable = new EnableGPSorWLAN(this);
+			
+			if(enable.isGPSenabled() == false && enable.WIFIenabled == true){
+				enable.enableGPS();	
+			}
+			if(enable.isWIFIEnabled()== false && enable.isGPSenabled() == true){
+				enable.enableNetwork();
+			}
+			if(enable.isWIFIEnabled()== false && enable.isGPSenabled() == false){
+				enable.enableAll();
+			}
+		}
 
+		public void placeMarker(double latitude, double longitude, String title){
+			
+			MarkerOptions marker = new MarkerOptions().position(new LatLng(latitude, longitude));
+			marker.title(title);
+			marker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+			map.addMarker(marker);
+		}
+		
+		public void setCameraPosition(double latitude, double longitude){
+			CameraPosition position = new CameraPosition.Builder().target(new LatLng(latitude, longitude)).zoom(15).build();
+			map.animateCamera(CameraUpdateFactory.newCameraPosition(position));
+		}
+		
+		public void makeCircle(double latitude, double longitude){
+			CircleOptions options = new CircleOptions();
+			options.center(new LatLng(latitude, longitude));
+			options.radius(10);
+			options.strokeColor(Color.parseColor("#00000000"));
+			options.fillColor(Color.parseColor("#99FF0000"));
+			this.map.addCircle(options);
+		}
+
+		
 }
